cba7e1e6970310e85b6e2d2e5458522251945417
Frederique Niemer <frederiqueniemer@241.148.028.145.hva.nl>
2019-04-24 09:41:06 +0200

    eerste versie html

---
+++ opdracht1/v1/javascript.js
@@ -0,0 +1,20 @@
+/*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
+/*eslint-env browser*/
+/*eslint 'no-console':0*/
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+            content.style.display = "none";
+        } else {
+            content.style.display = "block";
+        }
+    });
+}
+
+console.log('hallo werkt dit?');
